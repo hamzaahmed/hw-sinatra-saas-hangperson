1512cb6738681e72260b2c2542a4fdaf52ca8de0
hamzaahmed <hamza.ah@berkeley.edu>
2016-02-09 03:23:10 +0000

    Passed all tests for HangpersonGame

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,6 +3,9 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   # Get a word from remote "random word" service
 
   # def initialize()
@@ -10,6 +13,54 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = ''
+    @num_incorrect_guesses = 0
+  end
+
+  def letter?(input)
+    input =~ /[A-Za-z]/
+  end
+
+  def guess(letter)
+    if  (letter.nil?) || (letter.empty?) || !(letter?(letter))
+      raise ArgumentError
+    end
+    letter = letter.downcase
+    if (@guesses.include? letter) || (@wrong_guesses.include? letter)
+      return false
+    end
+    if @word.include? letter
+        @guesses << letter
+        @word_with_guesses = word_with_guesses
+    else
+        @wrong_guesses << letter
+        @num_incorrect_guesses += 1
+    end
+    check_win_or_lose
+  end
+
+  def word_with_guesses
+    @word_with_guesses = ''
+    @word.chars { |char|
+      if @guesses.include? char
+        @word_with_guesses += char
+      else
+        @word_with_guesses += '-'
+      end
+    }
+    return @word_with_guesses
+  end
+
+  def check_win_or_lose
+    if (@num_incorrect_guesses >= 7)
+      return :lose
+    elsif !(@word_with_guesses.include? '-')
+      return :win
+    else
+      return :play
+    end
   end
 
   def self.get_random_word
